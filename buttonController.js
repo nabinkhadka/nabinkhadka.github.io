d0b62dd8aa6893f8de5d10ff964d1de628bba789
Nabin Khadka <nbnnbnkhadka@gmail.com>
2016-11-03 20:15:57 +0545

    Add controller

---
+++ buttonController.js
@@ -0,0 +1,10 @@
+angular.module('MyApp',['ngMaterial', 'ngMessages'])
+
+.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+
+  $scope.googleUrl = 'http://google.com';
+
+});
