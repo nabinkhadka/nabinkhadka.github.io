75a6c6c5b94348e6706eda6c160c2dda4041a714
Nabin Khadka <nbnnbnkhadka@gmail.com>
2016-11-10 14:27:55 +0545

    Remove old desktop

--- desktop/build/temp/development/PersonalWebsite/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/Users/nabin_khadka/drive/projects/repos/nabinkhadka.github.io/desktop/build/.sass-cache'
--- desktop/build/temp/development/PersonalWebsite/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/nabin_khadka/drive/projects/repos/nabinkhadka.github.io/desktop/app.js
