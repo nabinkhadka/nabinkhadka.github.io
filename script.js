63db0f6b32f12e8a06e4c73c59b028829a3f1fc2
Nabin Khadka <nbnnbnkhadka@gmail.com>
2020-01-06 22:58:37 +0545

    Update script.js

--- script.js
+++ script.js
@@ -226,7 +226,7 @@ function showArchivesPage(){
 }
 
 function version() {
-    var version = "<span>nabin@Khadka:~$</span> version" + '<br><br>0.3 <br>Current update on: 17th Setpember 2019';
+    var version = "<span>nabin@Khadka:~$</span> version" + '<br><br>0.4 <br>Current update on: 6th Jan 2020';
     outputArea.innerHTML = version;
 
 }
